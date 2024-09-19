1c03eb109543739879d0306fb5d51b489457ba42
JaynAP1 <jerxonmjair@gmail.com>
2024-09-19 17:50:33 -0500

    :sparkles: Apliacion de la data en la maqueta 20%

--- script/Scrpt.js
+++ script/Scrpt.js
@@ -2,33 +2,104 @@ const NameVehicle = document.getElementById("NameVehicle");
 
 //Boxs
 const Box18 = document.querySelector(".Box18");
+const FirstProgressBar = document.querySelector(".FirstProgressBar");
+const SecondProgressBar = document.querySelector(".SecondProgressBar");
 
 fetch("https://api.spacexdata.com/v4/rockets")
 .then(Res =>Res.json())
 .then(Data =>{
-    console.log(Data)
+    
+    //Rocket Position
 
-    NameVehicle.textContent = Data[0].name
+    let Position = 0
 
+    //Operations
+
+    let KgVehicle = (Data[Position].mass.kg)/1000 +"kg";
+    let HeightVehicle = (Data[Position].diameter.meters)+"m"
+    let LowEarthOrbitVehicle = (Data[Position].payload_weights[0].kg)+"kg"
+    let DiameterRocketShield = (Data[Position].second_stage.payloads.composite_fairing.height.meters)+"m"
+    let HeightRocketShield = (Data[Position].second_stage.payloads.composite_fairing.diameter.meters)+"m"
+    let ThrustSeaLevel = Data[Position].first_stage.thrust_sea_level.kN+"Kn"
+    let ThrustVacuum = Data[Position].first_stage.thrust_vacuum.kN+"Kn"
+
+    //Name Vehicle
+    NameVehicle.textContent = Data[Position].name
+
+
+    //Content Boxes
     Box18.innerHTML = `
         <h2>Engine</h2>
         <HR style="border-color:#9499c3;"></HR>
         <div class="Text">
             <p class="MainText">Number</p>
-            <p>${Data[0].engines.number}</p>
+            <p>${Data[Position].engines.number}</p>
         </div>
         <div class="Text">
             <p class="MainText">Type</p>
-            <p>${Data[0].engines.type}</p>
+            <p>${Data[Position].engines.type}</p>
         </div>
         <div class="Text">
             <p class="MainText">Version</p>
-            <p>${Data[0].engines.version}</p>
+            <p>${Data[Position].engines.version}</p>
         </div>
         <div class="Text">
             <p class="MainText">Layout</p>
-            <p>${Data[0].engines.layout}</p>
+            <p>${Data[Position].engines.layout}</p>
         </div>
     `
 
+    FirstProgressBar.innerHTML = `
+        <div class="Box8">
+            <div class="Bar">
+                <p>Rocket Weight</p>
+                <div class="ProgressBar"></div>
+            </div>
+            <h3>${KgVehicle}</h3>  
+        </div>
+        <div class="Box9">
+            <div class="Bar">
+                <p>Low Earth Orbit</p>
+                <div class="ProgressBar"></div>
+            </div>
+            <h3>${LowEarthOrbitVehicle}</h3>  
+        </div>
+        <div class="Box10">
+            <div class="Bar">
+                <p>Rocket Diameter</p>
+                <div class="ProgressBar"></div>
+            </div>
+            <h3>${HeightVehicle}</h3>  
+        </div>
+        <div class="Box11">
+            <div class="Bar">
+                <p>Diameter Rocket Shield</p>
+                <div class="ProgressBar"></div>
+            </div>
+            <h3>${HeightRocketShield}</h3>  
+        </div>
+        <div class="Box12">
+            <div class="Bar">
+                <p>Height Rocket Shield</p>
+                <div class="ProgressBar"></div>
+            </div>
+            <h3>${DiameterRocketShield}</h3>  
+        </div>
+    `
+    SecondProgressBar.innerHTML = `
+        <div class="Box13">
+            <div class="Bar">
+                <p>Thrust Sea Level<p>
+                <div class="ProgressBar"></div>
+            </div>
+            <h3>${ThrustSeaLevel}</h3>
+        </div>
+        <div class="Box14">
+            <div class="Bar">
+                <p>Thrust Vacuum</p>
+                <div class="ProgressBar"></div>
+            </div>
+            <h3>${ThrustVacuum}</h3>
+        </div>
+    `
 })
