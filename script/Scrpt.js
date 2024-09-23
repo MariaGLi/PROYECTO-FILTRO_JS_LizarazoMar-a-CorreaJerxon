aeef88398a4cf65c68fffb50c715fd039e77e2a7
JaynAP1 <jerxonmjair@gmail.com>
2024-09-23 12:54:13 -0500

    :sparkles: Agregado del modal y funcionamiento de la data

--- script/Scrpt.js
+++ script/Scrpt.js
@@ -1,13 +1,18 @@
-
 //Boxs
+const Box17 = document.querySelector(".Box17");
 const Box18 = document.querySelector(".Box18");
 const FirstProgressBar = document.querySelector(".FirstProgressBar");
 const SecondProgressBar = document.querySelector(".SecondProgressBar");
 const HistoryContent = document.querySelector(".HistoryContent");
+const SoyModal = document.getElementById("Soymodal");
+const Browse = document.querySelectorAll(".Browse");
 
 //Objects
 const RocketImg = document.getElementById("Rocket");
 const NameVehicle = document.getElementById("NameVehicle");
+const Left = document.getElementById("Left");
+const Right = document.getElementById("Right");
+const BotonCerrarModal=document.getElementById("CerrarModal");
 
 //Icons
 const Check1 = document.getElementById("Check1");
@@ -18,75 +23,51 @@ const Check5 = document.getElementById("Check5");
 const Check6 = document.getElementById("Check6");
 const Check7 = document.getElementById("Check7");
 
-//Buttons
-const UpBtn = document.getElementById("UpImg");
-const DownBtn = document.getElementById("DownImg");
+//Variables
+let SumVariable = 0
+
+document.addEventListener("DOMContentLoaded",()=>{
+    SoyModal.showModal();
+    BotonCerrarModal.addEventListener("click",()=>{
+        SoyModal.close();
+    })
+})
 
 fetch("https://api.spacexdata.com/v4/rockets")
 .then(Res =>Res.json())
 .then(Data =>{
-    
-    //Rocket Position
-    let Position = 0
 
-    document.addEventListener("keydown",()=>{
-        Position+=1
+    Operations(SumVariable)
+    
+    document.addEventListener("keydown",(keys)=>{
+        if(keys.key === "ArrowRight"){
+            SumVariable +=1
+            Operations(SumVariable)
+        }
+        else if(keys.key === "ArrowLeft"){
+            SumVariable-=1
+            Operations(SumVariable)
+        }
+    })
+
+    //Navigation
+    Browse.forEach(Obj =>{
+        Obj.addEventListener("click",()=>{
+            Obj.style="background-color: #1e2031;"
+        })
     })
 
     //Operations
+    function Operations(Position){
+        let KgVehicle = (Data[Position].mass.kg)/1000 +"kg";
+        let HeightVehicle = (Data[Position].diameter.meters)+"m"
+        let LowEarthOrbitVehicle = (Data[Position].payload_weights[0].kg)+"kg"
+        let DiameterRocketShield = (Data[Position].second_stage.payloads.composite_fairing.height.meters)+"m"
+        let HeightRocketShield = (Data[Position].second_stage.payloads.composite_fairing.diameter.meters)+"m"
+        let ThrustSeaLevel = Data[Position].first_stage.thrust_sea_level.kN+"Kn"
+        let ThrustVacuum = Data[Position].first_stage.thrust_vacuum.kN+"Kn"
 
-    let KgVehicle = (Data[Position].mass.kg)/1000 +"kg";
-    let HeightVehicle = (Data[Position].diameter.meters)+"m"
-    let LowEarthOrbitVehicle = (Data[Position].payload_weights[0].kg)+"kg"
-    let DiameterRocketShield = (Data[Position].second_stage.payloads.composite_fairing.height.meters)+"m"
-    let HeightRocketShield = (Data[Position].second_stage.payloads.composite_fairing.diameter.meters)+"m"
-    let ThrustSeaLevel = Data[Position].first_stage.thrust_sea_level.kN+"Kn"
-    let ThrustVacuum = Data[Position].first_stage.thrust_vacuum.kN+"Kn"
-
-    let PositionImg = 0
-
-    //Name Vehicle
-    NameVehicle.textContent = Data[Position].name
-
-
-    //Content Boxes
-
-    HistoryContent.innerHTML = `<p>${Data[Position].description}</p>`;
-
-    RocketImg.src = Data[Position].flickr_images[PositionImg]
-
-    UpBtn.addEventListener("click",() => {
-        PositionImg += 1
-        RocketImg.src = Data[Position].flickr_images[PositionImg]
-    })
-    DownBtn.addEventListener("click",() =>{
-        PositionImg -= 1
-        RocketImg.src = Data[Position].flickr_images[PositionImg]
-    })
-    
-
-    Box18.innerHTML = `
-        <h2>Engine</h2>
-        <HR style="border-color:#9499c3;"></HR>
-        <div class="Text">
-            <p class="MainText">Number</p>
-            <p>${Data[Position].engines.number}</p>
-        </div>
-        <div class="Text">
-            <p class="MainText">Type</p>
-            <p>${Data[Position].engines.type}</p>
-        </div>
-        <div class="Text">
-            <p class="MainText">Version</p>
-            <p>${Data[Position].engines.version}</p>
-        </div>
-        <div class="Text">
-            <p class="MainText">Layout</p>
-            <p>${Data[Position].engines.layout}</p>
-        </div>
-    `
-
-    FirstProgressBar.innerHTML = `
+        FirstProgressBar.innerHTML = `
         <div class="Box8">
             <div class="Bar">
                 <p>Rocket Weight</p>
@@ -122,25 +103,79 @@ fetch("https://api.spacexdata.com/v4/rockets")
             </div>
             <h3>${DiameterRocketShield}</h3>  
         </div>
-    `
+        `
 
-    SecondProgressBar.innerHTML = `
-        <div class="Box13">
-            <div class="Bar">
-                <p>Thrust Sea Level<p>
-                <div class="ProgressBar6"></div>
+        SecondProgressBar.innerHTML = `
+            <div class="Box13">
+                <div class="Bar">
+                    <p>Thrust Sea Level<p>
+                    <div class="ProgressBar6"></div>
+                </div>
+                <h3>${ThrustSeaLevel}</h3>
             </div>
-            <h3>${ThrustSeaLevel}</h3>
-        </div>
-        <div class="Box14">
-            <div class="Bar">
-                <p>Thrust Vacuum</p>
-                <div class="ProgressBar7"></div>
+            <div class="Box14">
+                <div class="Bar">
+                    <p>Thrust Vacuum</p>
+                    <div class="ProgressBar7"></div>
+                </div>
+                <h3>${ThrustVacuum}</h3>
             </div>
-            <h3>${ThrustVacuum}</h3>
-        </div>
-    `
+        `
+        let PositionImg = 0
+
+        document.addEventListener("keydown",(keyss) => {
+            if(keyss.key === "ArrowUp"){
+                PositionImg +=1
+                RocketImg.src = Data[Position].flickr_images[PositionImg]
+            }
+            else if(keyss.key === "ArrowDown"){
+                PositionImg -=1
+                RocketImg.src = Data[Position].flickr_images[PositionImg]
+            }
+        })  
+        //Name Vehicle
+        NameVehicle.textContent = Data[Position].name
+
+        //Content Boxes
+
+        HistoryContent.innerHTML = `<p>${Data[Position].description}</p>`;
+
+        RocketImg.src = Data[Position].flickr_images[PositionImg]    
+        
+        Box17.innerHTML = `
+                <div class="Circle1" style="--Porcen: 120">
+                    <svg width="142px" heigth="152px">
+                        <circle r="50" cx="50%" cy="50%" pathlengh="100"></circle>
+                    </svg>
+                    <span>50%</span>
+                </div>
+                <div class="Circle2" style="--Porcen: 120">
+                    <svg width="142px" heigth="152px">
+                        <circle r="50" cx="50%" cy="50%" pathlengh="100"></circle>
+                    </svg>
+                    <span>50%</span>
+                </div>
+        `
+
+        Box18.innerHTML = `
+            <h2>Engine</h2>
+            <HR style="border-color:#9499c3;"></HR>
+            <div class="Text">
+                <p class="MainText">Number</p>
+                <p>${Data[Position].engines.number}</p>
+            </div>
+            <div class="Text">
+                <p class="MainText">Type</p>
+                <p>${Data[Position].engines.type}</p>
+            </div>
+            <div class="Text">
+                <p class="MainText">Version</p>
+                <p>${Data[Position].engines.version}</p>
+            </div>
+            <div class="Text">
+                <p class="MainText">Layout</p>
+                <p>${Data[Position].engines.layout}</p>
+            </div>
+        `
+        }   
 })
-function WaitsColor(){
-
-}
