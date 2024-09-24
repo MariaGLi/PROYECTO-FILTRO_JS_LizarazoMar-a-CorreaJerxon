d6d9bab1e30adc7607795866f05609c9e0ae2c88
MariaGLi <lizrazoleal27@gmail.com>
2024-09-23 22:19:49 -0500

    :construction: Solo falta hacerle responsive.

--- Script/Script_Factory.js
+++ Script/Script_Factory.js
@@ -5,20 +5,32 @@ function Company(){
     .then(res=> res.json())
     .then(data=>{
         console.log(data.ceo);
+
         let name = document.getElementById("NameCompany")
+        let text = document.getElementById("textCompany")
+        let ceo = document.getElementById("name")
+        let coo = document.getElementById("name2")
+        let cto = document.getElementById("name3")
+        let date = document.getElementById("date")
+
         name.innerHTML=`
         <h1 id="NameVehicle">${data.name}</h1>
         `
-        let text = document.getElementById("textCompany")
         text.innerHTML=`
         <p id="textCompany">${data.summary}</p>
         `
+        ceo.innerHTML=`
+        <p>${data.ceo}</p>
+        `
+        coo.innerHTML=`
+        <p>${data.coo}</p>
+        `
+        cto.innerHTML=`
+        <p>${data.cto_propulsion}</p>
+        `
+        date.innerHTML=`
+        <p>${data.founded}</p>
+        `
     })
-
-    list=["https://www.flickr.com/photos/spacex/51929288928/","https://www.flickr.com/photos/spacex/51829734959/","https://www.flickr.com/photos/spacex/51671874407/","https://www.flickr.com/photos/spacex/52218116121/","https://www.flickr.com/photos/spacex/52638916234/","https://www.flickr.com/photos/spacex/52553136344/","https://www.flickr.com/photos/spacex/52382109818/","https://www.flickr.com/photos/spacex/52822391709/","https://www.flickr.com/photos/spacex/52739186775/","https://www.flickr.com/photos/spacex/52687306805/"]
-    let img = document.getElementById("imagen")
-    img.src= list[0]
 }
 Company()
-
-/* https://66e45b6ed2405277ed14088d.mockapi.io/Space-X */
