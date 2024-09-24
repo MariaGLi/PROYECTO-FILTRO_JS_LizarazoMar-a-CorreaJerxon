b224da3dfa850aef197d7ee5000ef7d277b220e9
MariaGLi <lizarazoleal27@gmail.com>
2024-09-24 12:55:44 -0500

    :construction: For finish the page History

--- Script/Script_History.js
+++ Script/Script_History.js
@@ -8,12 +8,20 @@ function Company(){
 
         let title = document.getElementById("titleHistory")
         let text = document.getElementById("textHistory")
+        let unix =document.getElementById("date")
+        let utc =document.getElementById("date2")
 
         title.innerHTML=`
         <h1 id="titleHistory">${data[0].title}</h1>
         `
         text.innerHTML=`
         <p id="textHistory">${data[0].details}</p>
+        `
+        unix.innerHTML=`
+        <p id="textHistory">${data[0].event_date_unix}</p>
+        `
+        utc.innerHTML=`
+        <p id="textHistory">${data[0].event_date_utc}</p>
         `
     })
 }
